8f7ea19c0751eccc59f8ea06fe02d39b6884f7d7
Blaede Court-Suzuki <bcourtsu@ucsc.edu>
2024-02-28 15:43:43 -0800

    first commit

---
+++ src/scenes/Title.js
@@ -0,0 +1,42 @@
+class Title extends Phaser.Scene {
+    constructor() {
+        super("titleScene")
+    }
+
+    preload() {
+        // load assets
+        this.load.path = "./assets/"
+
+        // load JSON (ie dialog text)
+        this.load.json('dialog', 'json/dialog.json')
+
+        // load images
+        this.load.image('dialogbox', 'img/dialogbox.png')
+        this.load.image('homer', 'img/homer.png')
+        this.load.image('minerva', 'img/minerva.png')
+        this.load.image('jove', 'img/jove.png')
+        this.load.image('neptune', 'img/neptune.png')
+        this.load.image('naruto', 'img/naruto.jpg')
+
+        // load bitmap font
+        this.load.bitmapFont('gem_font', 'font/gem.png', 'font/gem.xml')
+        this.load.bitmapFont('mickey', 'font/mickey.png', 'font/mickey.xml')
+        //didn't call it _font because it's not going to be anything else in the game
+    }
+
+    create() {
+        // add title text
+        this.add.bitmapText(centerX, centerY - 32, 'mickey', 'THE ODYSSEY', 64).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY, 'mickey', 'Press SPACE to start', 32).setOrigin(0.5)
+
+        // create input
+        cursors = this.input.keyboard.createCursorKeys()
+    }
+
+    update() {
+        // wait for player input
+        if(Phaser.Input.Keyboard.JustDown(cursors.space)) {
+            this.scene.start("talkingScene")
+        }
+    }
+}
